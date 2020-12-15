4bf4a278162beea722c0320527fcb935a3829f15
BMO <bmo@skypress.io>
2020-12-15 12:35:06 -0600

    update tests and pg-mem

--- test/entity.spec.ts
+++ test/entity.spec.ts
@@ -1,7 +1,7 @@
 import { Connection, Repository } from 'typeorm';
 
 import { TempDB } from './test.db';
-import { Account } from '../src/entities/account.entity';
+import { Account, AccountType } from '../src/entities/account.entity';
 import { User } from '../src/entities/user.entity';
 
 describe('EntityTest', () => {
@@ -9,13 +9,13 @@ describe('EntityTest', () => {
   let account: Repository<Account>;
   let user: Repository<User>;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     connection = await TempDB([Account, User]);
     account = connection.getRepository(Account);
     user = connection.getRepository(User);
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await connection.close();
   });
 
@@ -23,4 +23,32 @@ describe('EntityTest', () => {
     expect(account).toBeDefined();
     expect(user).toBeDefined();
   });
+
+  it('should create an account', async () => {
+    const stub = {
+      name: 'test',
+      type: 'business' as AccountType,
+    };
+    const acct = account.create(stub);
+    expect(acct).toBeDefined();
+
+    const result = await account.save(acct);
+    expect(result).toHaveProperty('id');
+  });
+
+  it('should create a user', async () => {
+    const stub = {
+      email: 'test@domain.com',
+      account: account.create({name: 'test'})
+    }
+    stub.account = await account.save(stub.account)
+
+    const usr = user.create(stub)
+    expect(usr).toBeDefined()
+
+    const result = await user.save(usr)
+    expect(result).toBeDefined()
+    expect(result).toHaveProperty('id')
+    expect(result.account).toEqual(stub.account)
+  })
 });
